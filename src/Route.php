ac536282d1822bed111ceeb14edaffc0a40ad092
Wijnand de Ridder <wderidder@landstede.nl>
2025-12-03 13:26:17 +0100

    initial commit

---
+++ src/Route.php
@@ -0,0 +1,20 @@
+<?php
+namespace AlfPHP;
+class Route
+{
+
+    public function get($uri, $controller)
+    {
+
+    }
+
+    public function post($uri, $controller)
+    {
+
+    }
+
+    public function resource($uri, $controller, $paramNames)
+    {
+
+    }
+}
